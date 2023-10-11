e1c71c2adaf24e04d1162262b0e1405b23a052c3
Vasanthi Raja Gopal <gopalitech@gmail.com>
2023-10-11 09:23:38 -0700

    Initial commit for Hello world

--- src/main/java/com/poc/controller/HelloController.java
+++ src/main/java/com/poc/controller/HelloController.java
@@ -1,18 +1,21 @@
 package com.poc.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 @CrossOrigin
+@RequestMapping("/api/v1")
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Greetings from Spring Boot!";
+    public ResponseEntity<String> getHello() {
+        return ResponseEntity.ok().body("Greetings from Spring Boot!");
     }
 
 }
